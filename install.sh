1e3c2de19a0faaad890d091d8c11a15cf0157e76
Joerg Kastning <tronde@my-it-brain.de>
2013-12-16 16:27:25 +0100

    Checks eingebaut, um zu prüfen, ob Kommando korrekt ausgeführt wurde.

--- install.sh
+++ install.sh
@@ -3,20 +3,39 @@
 # Autor: Joerg Kastning
 # Voraussetzungen: Raspian OS on the Pi
 #
+# Funktionen
+function check() {
+  if [ $1 -gt 0 ]; then
+    !!
+    echo "Uuups, hier ist was schiefgegangen"
+    echo "exit $1"
+    exit 1
+  fi
+}
 # Installation der i1c-tools, lighttpd und php5-cgi
 apt-get update && apt-get -y install i2c-tools lighttpd php5-cgi
+check $?
 
 # I2C-Treiberaktivierung
 echo "i2c-dev" >>/etc/modules
+check 
 cp raspi-blacklist.conf /etc/modprobe.d/
 adduser pi i2c
-
+check $?
 # Einrichtung des Webservers und Aktivierung von FastCGI
 adduser pi www-data
+check $?
 chown -R www-data:www-data /var/www
+check $?
 chmod -R 775 /var/www
+check $?
 lighty-enable-mod fastcgi
+check $?
 /etc/init.d/lighttpd force-reload
+check $?
 cp lighttpd.conf /etc/lighttpd/
+check $?
 cp Raspi-SHT21-V3_0_0/www/* /var/www
+check $?
 ln -s ~/Raspi-SHT21-V3_0_0/sht21-data.csv sht21-data.csv
+check $?
