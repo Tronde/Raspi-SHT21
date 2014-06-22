ffab02d149acf8d22f4e435ee24b0591fa3f7f1b
Joerg Kastning <joerg.kastning@my-it-brain.de>
2014-06-22 15:26:39 +0200

    Fehler in der restart-Funktion des Start-Stop-Scripts behoben.

--- raspi-sht21.sh
+++ raspi-sht21.sh
@@ -55,7 +55,7 @@ case "$1" in
 	killall sht21.sh
 	echo "Daemon raspi-sht21 stopped."
 	echo "Starting raspi-sht21 daemon..."
-	cd /home/pi/Raspi-SHT21/Raspi-SHT21-V3_0_0/
+	cd /home/pi/Raspi-SHT21
 	if  ./start-sht21-service.sh; then
 		echo "Starting raspi-sht21 Daemon [OK]"
 	else
