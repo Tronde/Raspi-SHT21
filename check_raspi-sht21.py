d74421f6e30e4ecc5613ac802ed02e52aed8cb10
Joerg Kastning <joerg.kastning@my-it-brain.de>
2014-07-15 10:28:13 +0200

    Kommentare entfernt und Kosmetik

--- check_raspi-sht21.py
+++ check_raspi-sht21.py
@@ -11,12 +11,12 @@ def debug():
 	print min_humidity
 	print max_humidity
 
-parser = argparse.ArgumentParser(description=" Nagios Raspi-SHT21 Plugin. Dieses Plugin bestimmt die vom Raspi-SHT21 gemessene Temperatur und Luftfeuchtigkeit. Dieses Plugin bestimmt die vom Raspi-SHT21 gemessene Temperatur und Luftfeuchtigkeit und vergleicht die gemessenen Werte mit den übergebenen Grenzwerten für Temperatur und Luftfeuchtigkeit.")
+parser = argparse.ArgumentParser(description=" Nagios Raspi-SHT21 Plugin. Dieses Plugin bestimmt die vom Raspi-SHT21 gemessene Temperatur und Luftfeuchtigkeit und vergleicht die gemessenen Werte mit den übergebenen Grenzwerten für Temperatur und Luftfeuchtigkeit.")
 
 parser.add_argument("-t", "--min-temperature", dest="mintemperature", required=True, type=float, help="Untere Temperaturgrenze")
 parser.add_argument("-T", "--max-temperature", dest="maxtemperature", required=True, type=float, help="Obere Temperaturgrenze")
-parser.add_argument("--min-humidity", dest="minhumidity", required=True, type=int, help="Untere Grenze der Luftfeuchtigkeit")
-parser.add_argument("-H", "--max-humidity", dest="maxhumidity", required=True, type=int, help="Obere Grenze der Luftfeuchtigkeit")
+parser.add_argument("-l", "--min-humidity", dest="minhumidity", required=True, type=int, help="Untere Grenze der Luftfeuchtigkeit")
+parser.add_argument("-L", "--max-humidity", dest="maxhumidity", required=True, type=int, help="Obere Grenze der Luftfeuchtigkeit")
 parser.add_argument("-v", "--verbose", action="store_true", help="Aktiviert Debuggingausgabe.")
 
 args = parser.parse_args()
@@ -25,12 +25,6 @@ min_temperature = args.mintemperature
 max_temperature = args.maxtemperature
 min_humidity = args.minhumidity
 max_humidity = args.maxhumidity
-
-"""
-if min_temperature == None or max_temperature == None or min_humidity == None or max_humidity == None:
-	print_help()
-	parser.error("Es wurden nicht alle notwendigen Optionen angegeben.")
-"""
 
 temperature = float(os.popen("tail -n 1 sht21-data.csv | awk '{print $4}'").readline().strip())
 humidity = int(os.popen("tail -n 1 sht21-data.csv | awk '{print $5}'").readline().strip())
@@ -42,7 +36,7 @@ if temperature > min_temperature and temperature < max_temperature and humidity 
 	print "OK - Temperatur: %2.1f°C Luftfeuchtigkeit: %d%%. | temperature=%2.1f humidity=%d" % (temperature, humidity, temperature, humidity)
 	sys.exit(0)
 elif temperature < min_temperature or temperature > max_temperature or humidity < min_humidity or humidity > max_humidity:
-	print "WARNUNG - Temperatur: %2.1f°C Luftfeuchtigkeit: %d%%. | temperature=%2.1f humidity=%d" % (temperature, humidity, temperature, humidity)
+	print "WARNING - Temperatur: %2.1f°C Luftfeuchtigkeit: %d%%. | temperature=%2.1f humidity=%d" % (temperature, humidity, temperature, humidity)
 	sys.exit(1)
 else:
 	print "UNKNOWN"
