a9c6d686e1a31883c60c27054215dbb82d11efa8
Tronde <tronde@my-it-brain.de>
2016-01-25 20:42:53 +0100

    Berechnung ueberarbeitet. Schreiboperation fehlt noch.

--- calc_avgminmax.py
+++ calc_avgminmax.py
@@ -10,27 +10,26 @@
  Lizenz: GPLv3
 """
 
-import argparse, csv, sys
+import argparse
+import csv
+import sys
 
 sourcefile = 'sht21-data.csv'
-liste = []
+liste = []      # Nimmt den Inhalt von sourcefile auf
+firstitem = 0   # Erstes Element einer Liste
 lastitem = -1	# Bestimmt das letzte Element einer Liste
 tempitem = 2	# Bestimmt den Temperaturwert innerhalb der Liste
-humditem = 3	# Bestimmt den Luftfeuchtigkeitswert innerhalb der Liste
+humiditem = 3	# Bestimmt den Luftfeuchtigkeitswert innerhalb der Liste
 temperature = []
-min_temp = 0
-max_temp = 0
 humidity = []
-min_humid = 0
-max_humid = 0
 
 def debug():
     print ("Durchschnittstemperatur: %2.1f°C." % temp_avg)
-    print ("Minimum: %2.1f°C" % min_temp)
-    print ("Maximum: %2.1f°C" % max_temp)
-    print ("Durchschnittliche Luftfeuchtigkeit: %d%% RH." % humid_avg)
-    print ("Minimum: %d%% RH." % min_humid)
-    print ("Maximum: %d%% RH." % max_humid)
+    print ("Minimum: " + min_t[tempitem] + "°C")
+    print ("Maximum: " + max_t[tempitem] + "°C")
+    print ("Durchschnittliche Luftfeuchtigkeit: %d%% RH." % int(humid_avg))
+    print ("Minimum: " + min_h[humiditem] + " % RH")
+    print ("Maximum: " + max_h[humiditem] + " % RH")
 
 parser = argparse.ArgumentParser(description=" Dieses Script liest die vorhandene Datei 'sht21-data.csv' ein und berechnet Durschnitt, Minimum und Maximum für die aufgezeichnete Temperatur und Luftfeuchtigkeit.")
 
@@ -43,7 +42,25 @@ with open(sourcefile, 'r') as csvfile:
     for row in reader:
         liste.append(row)
         temperature.append(float(liste[lastitem][tempitem]))
-        humidity.append(int(liste[lastitem][humditem]))
+        humidity.append(float(liste[lastitem][humiditem]))
+
+min_t_helper = liste[firstitem][tempitem]
+max_t_helper = liste[firstitem][tempitem]
+min_h_helper = liste[firstitem][humiditem]
+max_h_helper = liste[firstitem][humiditem]
+for i in range(len(liste)-1):
+    if min_t_helper > liste[i+1][tempitem]:
+        min_t_helper = liste[i+1][tempitem]
+        min_t = liste[i+1]
+    if liste[i+1][tempitem] > max_t_helper:
+        max_t_helper = liste[i+1][tempitem]
+        max_t = liste[i+1]
+    if min_h_helper > liste[i+1][humiditem]:
+        min_h_helper = liste[i+1][humiditem]
+        min_h = liste[i+1]
+    if liste[i+1][humiditem] > max_h_helper:
+        max_h_helper = liste[i+1][humiditem]
+        max_h = liste[i+1]
 
 temp_sum = sum(temperature)
 temp_len = len(temperature)
@@ -51,19 +68,15 @@ temp_avg = temp_sum / temp_len
 humid_sum = sum(humidity)
 humid_len = len(humidity)
 humid_avg = humid_sum / humid_len
-min_temp = min(temperature)
-max_temp = max(temperature)
-min_humid = min(humidity)
-max_humid = max(humidity)
 
 if args.verbose:
     debug()
 
-def writestatsincsv(temp_avg,min_temp,max_temp,humid_avg,min_humid,max_humid):
-    row = temp_avg + "," + min_temp + "," + max_temp + "," + humid_avg + "," + min_humid + "," + max_humid + "\n"
+def writestatsincsv(temp_avg,min_t,max_t,humid_avg,min_h,max_h):
+    row = temp_avg + "," + min_t + "," + max_t + "," + humid_avg + "," + min_h + "," + max_h + "\n"
     f = open('statistic_data.csv', 'w')
     f.write(row)
     f.close()
-writestatsincsv(str("%2.1f" % temp_avg),str("%2.1f" % min_temp),str("%2.1f" % max_temp),str("%d" % humid_avg),str("%d" % min_humid),str("%d" % max_humid))
+writestatsincsv(str("%2.1f" % temp_avg),str(min_t),str(max_t),str("%d" % humid_avg),str(min_h),str(max_h))
 
 sys.exit(0)
