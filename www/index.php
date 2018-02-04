b19887cf267041706f62a9add13079045c7e97e3
Joerg Kastning <joerg.kastning@gmail.com>
2018-02-04 20:32:12 +0100

    Add autorefresh to index.php

--- www/index.php
+++ www/index.php
@@ -1,27 +1,34 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
+<?php
+$json = file_get_contents("sht21.json");
+$data = json_decode($json,TRUE);
+?>
 <head>
 <style>
 h1   {color:grey}
 p    {color:green}
 body    {font-family:verdana}
 </style>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="refresh" content="600">
+<title>
+  <?php
+    print("T: ".$data['temp']." &deg;C H: ".$data['humidity']." %");
+  ?>
+</title>
 </head>
 <body>
-<?php
+  <?php
+
+  //print_r($data);
+
+  print("<h1>".$data['time']." &nbsp;&nbsp;&nbsp;");
+  print("Temperature: ".$data['temp']." &deg;C &nbsp;&nbsp;&nbsp;");
+  print("Humidity: ".$data['humidity']." %</h1>");
 
 
-$json = file_get_contents("sht21.json");
-$data = json_decode($json,TRUE);
-
-//print_r($data);
-
-print("<h1>".$data['time']." &nbsp;&nbsp;&nbsp;");
-print("Temperature: ".$data['temp']." &deg;C &nbsp;&nbsp;&nbsp;");
-print("Humidity: ".$data['humidity']." %</h1>");
-
-
-?>
+  ?>
 	<img src="chart-day.png">
 	<img src="chart-week.png">
 </body>
