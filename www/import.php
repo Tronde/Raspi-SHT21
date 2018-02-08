3b66e457b4aba2503bf3855cfb194efc3fab60fd
Tronde <joerg.kastning@gmail.com>
2018-02-08 04:21:41 +0100

    Remove file rrd file form cache and add it to gitignore, testing version of import.php

--- www/import.php
+++ www/import.php
@@ -4,24 +4,33 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="refresh" content="600" >
     <title>
-	Import ESP8266
+	    Import ESP8266
     </title>
  </head>
 
  <body>
  <?php
-	$temp=$_GET["temp"];
-	$humd=$_GET["humd"];
-	$id=$_GET["id"];
-	$logentry = date('Y-m-d'). "T" . date('H:i:s', time()) . " " . $id . " " .  $temp . " " . $humd . "\n";
+  $name=$_POST["sysname"];
+  $taskname=$_POST["taskname"];
+	$id=$_POST["id"];
+	$temp=$_POST["Temperature"];
+	$humd=$_POST["Humidity"];
+	$logentry = date('Y-m-d'). "T" . date('H:i:s', time()) . " " . $name . " " .  $temp . " " . $humd . "\n";
+
+  echo("name: $name\n"); 
+  echo("taskname: $taskname\n"); 
+  echo("ID: $id\n"); 
+  echo("temp: $temp\n"); 
+  echo("humd: $humd\n"); 
 
 	print("<h1>". "Datenimport von ESP8266" ."</h1>");
-	print("<h2>". "Sensor-ID: ".$id."</h2>");
+	print("<h2>". "Sensor-ID: ".$name."</h2>");
 	print("<h3>". "Aktuelle  Temperatur: ".$temp."&deg;C<br />   Luftfeuchtigkeit: ".$humd."%</h3>");
+  print("<h3>". "Taskname: ".$taskname." ID: ".$id."</h3>");
 
-	$data = fopen("esp8266_data.csv","a");
-	fwrite($data, $logentry);
-	fclose($data);
+#	$data = fopen("esp8266_data.csv","a");
+#	fwrite($data, $logentry);
+#	fclose($data);
  ?>
  </body>
 </html>
